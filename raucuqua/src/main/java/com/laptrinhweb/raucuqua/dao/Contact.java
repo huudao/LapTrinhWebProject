e6acf6254f9150fae1475cb653cc5f76d53f5f6a
TranKhai2001 <19130094@st.hcmuaf.edu.vn>
2022-06-21 22:15:05 +0700

    thêm lớp contact chứa phương thức lưu contact vào database, sử dụng được file .config

---
+++ raucuqua/src/main/java/com/laptrinhweb/raucuqua/dao/Contact.java
@@ -0,0 +1,34 @@
+package com.laptrinhweb.raucuqua.dao;
+
+import com.laptrinhweb.raucuqua.connection.GetConnection;
+import com.laptrinhweb.raucuqua.dao.Util;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Contact {
+    public static void addContact(String email,String name,String phone_name,String content) {
+        try {
+            Connection con = GetConnection.getCon();
+            String nextID = Util.nextID(con, "contact", "id_contact", "CT");
+            String sql = "insert into contact values (?,?,?,?,?)";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, nextID);
+            ps.setString(2, email);
+            ps.setString(3, name
+            );
+            ps.setString(4, phone_name);
+            ps.setString(5, content);
+            int clar = ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        addContact("trkih2001@gmail.com","Khai","0901197448","alooooo");
+    }
+
+}
