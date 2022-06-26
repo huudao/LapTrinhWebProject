354b1ca48540df1f0609fe0cac810004c82b46a8
TranKhai2001 <19130094@st.hcmuaf.edu.vn>
2022-06-26 20:58:46 +0700

    cập nhật lớp CartFuntion
    thêm phương thức checkProductCart để kiểm tra số lần xuất hiện của id_user có id_product, thêm phương thức insertProduc để thêm số lương của sản phẩm

--- raucuqua/src/main/java/com/laptrinhweb/raucuqua/dao/CartFuntions.java
+++ raucuqua/src/main/java/com/laptrinhweb/raucuqua/dao/CartFuntions.java
@@ -5,12 +5,16 @@ import com.laptrinhweb.raucuqua.connection.GetConnection;
 import com.laptrinhweb.raucuqua.beans.Product;
 import com.laptrinhweb.raucuqua.beans.UserAccount;
 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.laptrinhweb.raucuqua.dao.Util.checkDataExist;
+import static com.laptrinhweb.raucuqua.dao.Util.check_amount_in_warehouse;
 
 public class CartFuntions {
     public static List<Cart> watch(String id_user){
@@ -52,5 +56,66 @@ public class CartFuntions {
             e.printStackTrace();
         }
         return null;
+
+    }
+    public static boolean checkProductCart(String id_user,String id_product){
+        try {
+            Connection con = GetConnection.getCon();
+            String sql = "select * from cart where id_user = ? and id_product = ?";
+            PreparedStatement ps  = con.prepareStatement(sql);
+            ps.setString(1,id_user);
+            ps.setString(2,id_product);
+            ResultSet rs = ps.executeQuery();
+            int count = 0;
+            while(rs.next()){
+                count++;
+            }
+            rs.close();
+            if(count==1){
+                return true;
+            }
+            return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    //duplicate entry -> using updateAmount() method instead
+    public static boolean insertProduct(String id_user,String id_product,int amount) {
+        try {
+            System.out.println("rumme0");
+            if(amount<0){return false;}
+            System.out.println("rumme1");
+            Connection con = null;
+            con = GetConnection.getCon();
+            if (!checkDataExist("id_user", id_user, "user_account")) return false;
+            System.out.println("rumme2");
+
+            if (!checkDataExist("id_product", id_product, "product")) return false;
+            System.out.println("rumme3");
+
+            check_amount_in_warehouse(con, id_product, amount);
+            String sql = "insert into cart(id_user,id_product,c_amount_bought) values(?,?,?)";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, id_user);
+            ps.setString(2, id_product);
+            ps.setInt(3, amount);
+            int scalar = ps.executeUpdate();
+            System.out.println("rumme4 scalar"+scalar);
+
+            if (scalar == 1) return true;
+            else return false;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void main(String[] args) {
+        watch("UA0001");
+        insertProduct("UA0001","PD0001",10);
+        System.out.println(checkProductCart("UA0001","PD0001"));
     }
 }
