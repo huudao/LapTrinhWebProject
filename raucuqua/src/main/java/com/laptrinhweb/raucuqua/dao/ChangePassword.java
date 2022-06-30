2d19d41f350ef969a58a3026c17c110178a81d5c
unknown <thuan0373535207@gmail.com>
2022-06-30 22:59:50 +0700

    cập nhật phương thức thanh toán. quên mật khẩu
    kiểm tra xem email có tồn tại không, nếu có -> thay đổi mật khẩu thông qua email
    Signed-off-by: unknown <thuan@gmail.com>

--- raucuqua/src/main/java/com/laptrinhweb/raucuqua/dao/ChangePassword.java
+++ raucuqua/src/main/java/com/laptrinhweb/raucuqua/dao/ChangePassword.java
@@ -25,4 +25,25 @@ public class ChangePassword {
         }
         return false;
     }
+    public static boolean changePassByEmail(String email,String newPassword){
+        try {
+            Connection con = GetConnection.getCon();
+            String sql = "UPDATE user_account set password=? where email = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1,newPassword);
+            ps.setString(2,email);
+            int result = ps.executeUpdate();
+            if(result >=1){
+                return true;
+            }else{
+                return false;
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(changePassByEmail("thuan0373535207@gmail.com","hey javax"));
+    }
 }
